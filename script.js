2125c544f4edb50fd0f20bfe43c32784f806b8f6
Eli Dawson <edawson@alumni.berklee.edu>
2023-04-24 14:54:42 -0400

    Fixed get request to recieve player info

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'cd31b3d6f5msh3ddc4783abc2071p1ae416jsn4958d92b9184',
-		'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
-	}
-};
+document.addEventListener('DOMContentLoaded', () => { 
 
-fetch('https://api-nba-v1.p.rapidapi.com/seasons', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+	const url = 'https://api-nba-v1.p.rapidapi.com/players?team=1&season=2021';
+	const options = { // declaring the options for the get request
+		method: 'GET',
+		headers: {
+			'content-type': 'application/octet-stream',
+			'X-RapidAPI-Key': 'cd31b3d6f5msh3ddc4783abc2071p1ae416jsn4958d92b9184',
+			'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
+		}
+	};
+	
+	fetch(url, options)
+		.then(response => response.json())
+		.then(response => console.log(response))
+		.catch(err => console.error(err));
+	
+	
+	})
+	
